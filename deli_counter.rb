c2b3e87c86c6b3ed0e977aac20b2bf12bf00d69b
Rafael Vargas <vargas718@gmail.com>
2019-01-17 14:52:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+
+def line(katz_deli)
+ counter = 1
+  if katz_deli.count == 0 
+    puts "The line is currently empty."
+  else
+    result = "The line is currently: "
+ names = []
+ katz_deli.each do |cust| 
+ #binding.pry
+   names.push("#{counter}. #{cust}" )
+   counter += 1
+  end
+puts result += names.join(" ")
+end
+
+end
+def take_a_number(katz_deli, names)
+  
+  katz_deli << #{"names"}
+  
+  puts "Welcome, #{"cust"}. You are number 1 in line."
+end
+
+  
